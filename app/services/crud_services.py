7ee9470fdbcd008b2c6e87f1e0c9c8528be2ee26
Svetlana Sycheva <svetlanasych@gmail.com>
2024-04-02 18:06:55 +0200

    All to service

--- app/services/crud_services.py
+++ app/services/crud_services.py
@@ -1,32 +1,14 @@
-from fastapi import HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime
+from typing import Type
 
+from fastapi import HTTPException
+from sqlalchemy import select
+
+from app.core.constants import DEFAULT_AMOUNT
 from app.crud.charityproject import charity_project_crud
 from app.crud.donation import donation_crud
 from app.models import Donation, CharityProject
-from app.services.investing import investing, close_obj
-from app.services.validators import check_charity_project_before_delete, check_charity_project_fully_invested
-
-
-async def _check_name_duplicate(
-        project_name: str,
-        session: AsyncSession,
-) -> None:
-    project_id = await charity_project_crud.get_project_id_by_name(project_name, session)
-    if project_id is not None:
-        raise HTTPException(
-            status_code=400,
-            detail='Проект с таким именем уже существует!')
-
-
-def _check_full_amount(
-    full_amount: int,
-    invested_amount: int
-) -> None:
-    if full_amount < invested_amount:
-        raise HTTPException(
-            status_code=400,
-            detail='Требуемая сумма не может быть меньше уже внесённой суммы!')
+from app.models.base import BothModel
 
 
 class InvestmentManager:
@@ -34,21 +16,99 @@ class InvestmentManager:
     def __init__(self, session):
         self.session = session
 
+    def close_obj(obj: Type[BothModel]) -> BothModel:
+        obj.invested_amount = obj.full_amount
+        obj.fully_invested = True
+        obj.close_date = datetime.now()
+        return obj
+
+    def calculate_invested_amount(obj_in, obj):
+        available = obj_in.full_amount - obj_in.invested_amount
+        expected = obj.full_amount - obj.invested_amount
+        if expected == available:
+            obj_in = InvestmentManager.close_obj(obj_in)
+            obj = InvestmentManager.close_obj(obj)
+        if available > expected:
+            obj_in.invested_amount += expected
+            obj = InvestmentManager.close_obj(obj)
+        if available < expected:
+            obj.invested_amount += available
+            obj_in = InvestmentManager.close_obj(obj_in)
+        return obj_in, obj
+
+    async def investing(
+        self,
+        obj_in: Type[BothModel],
+        model: Type[BothModel],
+    ) -> None:
+        open_objs = await self.execute(select(model).where(
+            model.fully_invested == 0
+        ).order_by(model.create_date))
+        open_objs = open_objs.scalars().all()
+        for obj in open_objs:
+            obj_in, obj = InvestmentManager.calculate_invested_amount(obj_in, obj)
+            self.add(obj_in)
+            self.add(obj)
+        await self.commit()
+        await self.refresh(obj_in)
+        return obj_in
+
+    async def _check_name_duplicate(
+        project_name: str,
+        self,
+    ) -> None:
+        project_id = await charity_project_crud.get_project_id_by_name(project_name, self)
+        if project_id is not None:
+            raise HTTPException(
+                status_code=400,
+                detail='Проект с таким именем уже существует!')
+
+    def _check_full_amount(
+        full_amount: int,
+        invested_amount: int
+    ) -> None:
+        if full_amount < invested_amount:
+            raise HTTPException(
+                status_code=400,
+                detail='Требуемая сумма не может быть меньше уже внесённой суммы!')
+
+    def check_charity_project_fully_invested(
+            project: CharityProject
+    ) -> None:
+        if project and project.fully_invested:
+            raise HTTPException(
+                status_code=400,
+                detail='Закрытый проект нельзя редактировать!')
+
+    def check_charity_project_before_delete(
+            project: CharityProject
+    ) -> None:
+        if project.fully_invested:
+            raise HTTPException(
+                status_code=400,
+                detail='В проект были внесены средства, не подлежит удалению!'
+            )
+        if project.invested_amount != DEFAULT_AMOUNT:
+            raise HTTPException(
+                status_code=400,
+                detail='В проект были внесены средства, не подлежит удалению!'
+            )
+
     async def update_partial_charity_project(self, project, obj_in):
         if obj_in.name != project.name:
-            await _check_name_duplicate(obj_in.name, self)
+            await InvestmentManager._check_name_duplicate(obj_in.name, self)
         if obj_in.full_amount != project.full_amount:
-            _check_full_amount(
+            InvestmentManager._check_full_amount(
                 obj_in.full_amount, project.invested_amount)
-        check_charity_project_fully_invested(project)
+        InvestmentManager.check_charity_project_fully_invested(project)
         if project.full_amount == project.invested_amount:
-            close_obj(project)
+            InvestmentManager.close_obj(project)
         project = await charity_project_crud.update(
             project, obj_in, self)
         return project
 
     async def remove_charity_project(self, project):
-        check_charity_project_before_delete(
+        InvestmentManager.check_charity_project_before_delete(
             project)
         await charity_project_crud.remove(
             project, self
@@ -56,13 +116,13 @@ class InvestmentManager:
         return project
 
     async def create_full_charity_project(self, project):
-        await _check_name_duplicate(project.name, self)
+        await InvestmentManager._check_name_duplicate(project.name, self)
         new_project = await charity_project_crud.create(self, project)
-        new_project = await investing(self, new_project, Donation)
+        new_project = await InvestmentManager.investing(self, new_project, Donation)
         return new_project
 
     async def create_full_donation(self, donation, user):
         new_donation = await donation_crud.create(
             self, donation, user)
-        new_donation = await investing(self, new_donation, CharityProject)
+        new_donation = await InvestmentManager.investing(self, new_donation, CharityProject)
         return new_donation
